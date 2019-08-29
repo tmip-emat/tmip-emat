ed93ac60e5a6f26ecb933a2c69fb2fd7f6781ace
Jeff Newman <jeff@newman.me>
2019-08-28 21:34:35 -0500

    let multioutput have only one output

--- emat/learn/multioutput.py
+++ emat/learn/multioutput.py
@@ -1,5 +1,5 @@
 
-
+import pandas
 from sklearn.multioutput import MultiOutputRegressor as _MultiOutputRegressor
 from .frameable import FrameableMixin
 from .model_selection import CrossValMixin
@@ -8,6 +8,10 @@ class MultiOutputRegressor(_MultiOutputRegressor, FrameableMixin, CrossValMixin)
 
 	def fit(self, X, y, sample_weight=None):
 		self._pre_fit(X,y)
+		if isinstance(y, pandas.Series):
+			y = y.to_frame()
+		elif y.ndim == 1:
+			y = y.reshape(-1,1)
 		return super().fit(X, y, sample_weight=sample_weight)
 
 	def predict(self, X):
