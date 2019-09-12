92166f1e8c4a44aa11a9ac611d0c13997cd2b4bd
Jeffrey Newman <jeff@newman.me>
2019-09-12 07:51:10 -0500

    include robustness_functions in OptimizationResult

--- emat/optimization/optimization_result.py
+++ emat/optimization/optimization_result.py
@@ -3,14 +3,16 @@ from ..viz.parcoords import ParCoordsViewer
 
 class OptimizationResult:
 
-	def __init__(self, result, convergence, scope=None):
+	def __init__(self, result, convergence, scope=None, robustness_functions=None):
 		self.result = result
 		self.convergence = convergence
 		self.scope = scope
+		self.robustness_functions = robustness_functions
 
 	def par_coords(self):
 		return ParCoordsViewer(
 			self.result,
-			self.scope,
+			scope=self.scope,
+			robustness_functions=self.robustness_functions,
 		)
 
