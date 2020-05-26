5d59f3f951ef66e04282a69db7aec5f11d5397e2
Jeff Newman <jeffnewman@camsys.com>
2020-05-26 08:02:31 -0500

    print_worker_logs for util.distributed

--- emat/util/distributed.py
+++ emat/util/distributed.py
@@ -17,3 +17,14 @@ def get_client(n_workers=4, threads_per_worker=1):
 		_client = Client(_cluster)
 	return _client
 
+def print_worker_logs(client=None):
+	if client is None:
+		client = get_client()
+	logs = client.get_worker_logs()
+	for worker_ip, messages in logs.items():
+		print("=" * 60)
+		print(worker_ip)
+		print("=" * 60)
+		for i in messages:
+			print(i[1])
+	print("=" * 60)
