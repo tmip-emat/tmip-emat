1546b0c2b7d890d941eaa1deb457444abe3a31ca
Jeff Newman <jeffnewman@camsys.com>
2021-03-03 09:59:27 -0600

    partial metamodels doc

--- docs/source/emat.metamodels/creation.py
+++ docs/source/emat.metamodels/creation.py
@@ -19,6 +19,7 @@
 
 # %%
 import emat
+from emat.util.loggers import timing_log
 emat.versions()
 
 # %% [markdown]
@@ -39,8 +40,10 @@ results = model.run_experiments(design)
 # We can then create a meta-model automatically from these experiments.
 
 # %%
-mm = model.create_metamodel_from_design('lhs', suppress_converge_warnings=True)
-mm
+from emat.model import create_metamodel
+
+with timing_log("create metamodel"):
+    mm = create_metamodel(scope, results, suppress_converge_warnings=True)
 
 # %% [markdown]
 # If you are using the default meta-model regressor, as we are doing here, 
@@ -50,7 +53,8 @@ mm
 # outcomes, similar to an $R^2$ measure on a linear regression model.
 
 # %%
-mm.cross_val_scores()
+with timing_log("crossvalidate metamodel"):
+    display(mm.cross_val_scores())
 
 # %% [markdown]
 # We can apply the meta-model directly on a new design of experiments, and 
@@ -58,11 +62,58 @@ mm.cross_val_scores()
 # meta-model is replicating the underlying model's results.
 
 # %%
-design2 = mm.design_experiments(design_name='lhs_meta', n_samples=5000)
+design2 = mm.design_experiments(design_name='lhs_meta', n_samples=10_000)
 
 # %%
-results2 = mm.run_experiments(design2)
+with timing_log("apply metamodel"):
+    results2 = mm.run_experiments(design2)
+
+# %%
+results2.info()
 
 # %%
 from emat.analysis import contrast_experiments
 contrast_experiments(mm.scope, results2, results)
+
+# %% [markdown]
+# ## Partial Metamodels
+
+# %% [markdown]
+# It may be desirable in some cases to construct a *partial* metamodel, covering only a subset of the performance measures.  This is likely to be particularly desirable if a large number of performance measures are included in the scope, but only a few are of particular interest for a given analysis. The time required for generating and using meta-models is linear in the number of performance measures included, so if you have 100 performance measures but you are only presently interested in 5, your meta-model can be created much faster if you only include the 5 performance measures.  It will also run much faster, but the run time for metamodels is so small anyhow, it's likely you won't notice.
+#
+# To create a partial meta-model for a curated set of performance measures, you can use the `include_measures` argument of the `create_metamodel` command.
+
+# %%
+with timing_log("create limited metamodel"):
+    mm2 = create_metamodel(
+        scope, results, 
+        include_measures=['time_savings', 'present_cost_expansion'],
+        suppress_converge_warnings=True,
+    )
+
+with timing_log("crossvalidate limited metamodel"):
+    display(mm2.cross_val_scores())
+
+with timing_log("apply limited metamodel"):
+    results2_limited = mm2.run_experiments(design2)
+    
+results2_limited.info()
+
+# %% [markdown]
+# There is also an `exclude_measures` argument for the `create_metamodel` command, which will retain all of the scoped performance measures *except* the enumerated list.  This can be handy for dropping a few measures that are not working well, either because the data is bad in some way or if the measure isn't well fitted using the metamodel.
+
+# %%
+with timing_log("create limited metamodel"):
+    mm3 = create_metamodel(
+        scope, results, 
+        exclude_measures=['net_benefits'],
+        suppress_converge_warnings=True,
+    )
+
+with timing_log("crossvalidate limited metamodel"):
+    display(mm3.cross_val_scores())
+
+with timing_log("apply limited metamodel"):
+    results3_limited = mm3.run_experiments(design2)
+    
+results3_limited.info()
