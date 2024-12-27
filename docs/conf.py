9a4a7a23e81237735b137e72af0b9a2c4f0e3252
Jeff Newman <jeff@driftless.xyz>
2024-12-27 17:01:19 -0600

    nbsphinx_custom_formats

--- docs/conf.py
+++ docs/conf.py
@@ -74,6 +74,10 @@ extensions = [
     'jupyter_sphinx',
 ]
 
+nbsphinx_custom_formats = {
+    ".py": ["jupytext.reads", {"fmt": "py:percent"}],
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
